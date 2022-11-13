f31ce3c2129bff4f554055895bb9a0823a028520
Artamananda <artamananda@gmail.com>
2022-11-14 00:36:47 +0700

    init project

---
+++ app/src/main/java/unsri/bikeus/LoginActivity.java
@@ -0,0 +1,17 @@
+package unsri.bikeus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
