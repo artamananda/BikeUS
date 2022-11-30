11c5e3cc816348de5a37fbfe5bd588c993d3e0f7
Artamananda <artamananda@gmail.com>
2022-11-30 22:37:49 +0700

    add menu bar

--- app/src/main/java/unsri/bikeus/MainActivity.java
+++ app/src/main/java/unsri/bikeus/MainActivity.java
@@ -1,14 +1,44 @@
 package unsri.bikeus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import unsri.bikeus.databinding.ActivityMainBinding;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new BicycleFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.bicycle:
+                    replaceFragment(new BicycleFragment());
+                    break;
+                case R.id.account:
+                    replaceFragment(new AccountFragment());
+                    break;
+                case R.id.history:
+                    replaceFragment(new HistoryFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
